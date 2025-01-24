7ed15ed8efe6f060d668b788beb6a4d0520b75c6
Marcus Odin <marodi@kth.se>
2025-01-24 14:49:07 +0100

    feat: code skeleton #1

---
+++ Decide.java
@@ -0,0 +1,59 @@
+import java.awt.geom.Point2D;
+
+public class Decide {
+    public static enum CONNECTORS{
+        ANDD,
+        ORR,
+        NOTUSED
+    }
+
+    // Inputs to the DECIDE() function
+    public class PARAMETERS_T {
+        double LENTH1;          
+        double RADIUS1;
+        double EPSILON;
+        double AREA1;
+        int Q_PTS;
+        int QUADS;
+        double DIST;
+        int N_PTS;
+        int K_PTS;
+        int A_PTS;
+        int B_PTS;
+        int C_PTS;
+        int D_PTS;
+        int E_PTS;
+        int F_PTS;
+        int G_PTS;
+        double LENTH2;
+        double RADIUS2;
+        double AREA2;
+    }
+
+    public PARAMETERS_T PARAMETERS;
+
+    // 100 x and y coordinates
+    public Point2D[] COORDINATES = new Point2D[100];
+
+    // Logical connector matrix (LCM)
+    public CONNECTORS[][] LCM = new CONNECTORS[15][15];
+
+    // Preliminary Unlocking Vector (PUV)
+    public boolean[] PUV = new boolean[15];
+
+    // Preliminary Unlocking Matrix (PUM)
+    boolean[][] PUM = new boolean[15][15];
+
+    // Conditions Met Vecttor (CMV)
+    boolean []CMV = new boolean[15];
+
+    // Final Unlocking Vector (FUV)
+    boolean[] FUV = new boolean[15];
+
+    // Decision: Launch or Not Launch
+    boolean launch;
+
+
+
+
+}
